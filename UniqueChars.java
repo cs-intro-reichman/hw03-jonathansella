ea31dc9e47d33cb0a3a5ea45e612eb65067d5dd0
unknown <jonathansella1@gmail.com>
2024-01-07 19:55:13 +0200

    homework 3 - ex 2

--- UniqueChars.java
+++ UniqueChars.java
@@ -10,8 +10,18 @@ public class UniqueChars {
      * except that all the duplicate characters are removed,
      * unless they are space characters.
      */
-    public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+    public static String uniqueChars(String s1String) {
+        String s = "";
+     for (int i = 0; i < s1String.length(); i++) {
+        if (s1String.charAt(i) == 32) {
+            s = s + (char) 32;
+        } else if (s.indexOf(s1String.charAt(i)) == -1) {
+            s = s + s1String.charAt(i);
+        } 
+
+        }
+         return s;
+        }
+        
     }
-}
+
